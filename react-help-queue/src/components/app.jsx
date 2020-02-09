51e7a42b40f4efdbd3a2a79523a8ef6fc6a8dafa
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-02-09 14:46:48 -0800

    add further fucntionality

---
+++ react-help-queue/src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header"
+
+function App(){
+  return (
+    <div>
+      <h1>Help Queue</h1>
+      <h3>6a</h3>
+      <h3>Thato and Haley</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
